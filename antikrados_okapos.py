045ae34cc229b3604a4fb279676399fbe43e32d1
Viutla <vitjuricek@gmail.com>
2024-04-11 13:40:05 +0200

    pridany materialy a if

--- antikrados_okapos.py
+++ antikrados_okapos.py
@@ -2,4 +2,6 @@ def zabran_kradezi_okapu(material_okapu):
     materialy = ["plast","ocel","hliník"]
     if material_okapu in materialy:
         print( "Okap se nevyplatí ukrást")
+    else:
+        print("Ber to moreee")
 zabran_kradezi_okapu("ocel")
